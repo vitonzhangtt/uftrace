bbbf113ef0d0e43b5662b7496d8d79b18a5eae42
Namhyung Kim <namhyung@gmail.com>
2018-11-14 16:13:28 +0900

    test: Add a test case for signal trigger
      $ uftrace -a --signal SIGUSR1@finish tests/t-signal
      # DURATION     TID     FUNCTION
                  [ 13635] | main(1, 0x7ffd5c074428) {
         0.245 us [ 13635] |   foo() = 0;
         1.572 us [ 13635] |   signal(SIGUSR1, &sighandler) = 0x7f20827b9fd0;
                  [ 13635] |   raise() {
                  [ 13635] |     sighandler(10) {
         0.102 us [ 13635] |       bar(10);
                  [ 13635] |       /* linux:task-exit */
    
      uftrace stopped tracing with remaining functions
      ================================================
      task: 13635
      [2] sighandler
      [1] raise
      [0] main
    
    Signed-off-by: Namhyung Kim <namhyung@gmail.com>

---
+++ tests/t212_signal_trigger.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+from runtest import TestBase
+
+class TestCase(TestBase):
+    def __init__(self):
+        TestBase.__init__(self, 'signal', """
+# DURATION     TID     FUNCTION
+            [ 11892] | main() {
+   0.241 us [ 11892] |   foo();
+   1.611 us [ 11892] |   signal();
+            [ 11892] |   raise() {
+            [ 11892] |     sighandler() {
+   0.120 us [ 11892] |       bar();
+
+uftrace stopped tracing with remaining functions
+================================================
+task: 11892
+[2] sighandler
+[1] raise
+[0] main
+""")
+
+    def runcmd(self):
+        uftrace = TestBase.uftrace_cmd
+        options = "--signal SIGUSR1@finish"
+        program = 't-' + self.name
+
+        return "%s %s %s" % (uftrace, options, program)
